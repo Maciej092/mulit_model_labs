ed3d8b74f551673c694054857e48326c582e42ad
macius092 <maciej.skorski092@gmail.com>
2019-11-27 07:00:19 +0100

    Add 'absorbing' boundary rule

--- ca_algorithm.py
+++ ca_algorithm.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import random
-# fig = plt.figure()
-# data = np.array([[np.zeros(3) for item in range(y)] for itemm in range(x)])
-# im = plt.imshow(data, animated=True)
+import time
+
+
 neighbour_rule = 'vonNeumann'
+border_rule = 'absorbing'    # OR 'absorbing'
 space_width = 50
 space_lengtth = 50
 cell_empty = np.array([255, 255, 255], dtype=int)
@@ -14,7 +15,7 @@ array = [[cell_empty for y in range(space_width)] for x in range(space_lengtth)]
 space = np.array(array)
 
 fig = plt.figure()
-l, = plt.imshow(space)
+im = plt.imshow(space, animated=True)
 
 
 def add_random(number):
@@ -24,23 +25,31 @@ def add_random(number):
         space[x, y] = np.random.randint(0,255,3)
     plt.imshow(space)
 
+
 def add():
     space[0, 1] = np.array([0,0,0])
     plt.imshow(space)
 
+
 def one_step(space):
-    space_new = np.array(array)
+    t1 = time.time()
+    space_new = np.array([[cell_empty for y in range(space_width)] for x in range(space_lengtth)])
     for x in range(space_width):
         for y in range(space_lengtth):
-            cell_current = space[x, y]
-            if (cell_current<cell_empty).any():
-                space_new[x, y] = cell_current
+            if (space[x, y]<cell_empty).any():
+                space_new[x, y] = space[x, y]
                 continue
             else:
                 neighbour = check_nieghbours(x, y)
                 neighbour_dict = {}
                 for neigh in neighbour:
-                    cell_neigh = space[neigh[0], neigh[1]]
+                    if border_rule != 'snakelike':
+                        if neigh[0] < 0 or neigh[1] < 0 or neigh[0] == space_width or neigh[1] == space_lengtth:
+                            cell_neigh = cell_empty
+                        else:
+                            cell_neigh = space[neigh[0], neigh[1]]
+                    else:
+                        cell_neigh = space[neigh[0], neigh[1]]
                     if (cell_neigh<cell_empty).any():
                         temp_val = neighbour_dict.setdefault(tuple(cell_neigh),0)
                         temp_val += 1
@@ -56,6 +65,7 @@ def one_step(space):
                 else:
                     out_val = cell_empty
             space_new[x, y] = out_val
+    print(f"{time.time() - t1}")
     plt.imshow(space_new)
     return space_new
 
@@ -63,20 +73,36 @@ def one_step(space):
 def check_nieghbours(x, y):
     if neighbour_rule == 'Moore':
         neighbour    = []
-        neighbour.append(((x - 1)%space_width, (y - 1)%space_lengtth))
-        neighbour.append(((x - 1)%space_width, (y    )%space_lengtth))
-        neighbour.append(((x - 1)%space_width, (y + 1)%space_lengtth))
-        neighbour.append(((x    )%space_width, (y - 1)%space_lengtth))
-        neighbour.append(((x    )%space_width, (y + 1)%space_lengtth))
-        neighbour.append(((x + 1)%space_width, (y - 1)%space_lengtth))
-        neighbour.append(((x + 1)%space_width, (y    )%space_lengtth))
-        neighbour.append(((x + 1)%space_width, (y + 1)%space_lengtth))
+        if border_rule == 'snakelike':
+            neighbour.append(((x - 1)%space_width, (y - 1)%space_lengtth))
+            neighbour.append(((x - 1)%space_width, (y    )%space_lengtth))
+            neighbour.append(((x - 1)%space_width, (y + 1)%space_lengtth))
+            neighbour.append(((x    )%space_width, (y - 1)%space_lengtth))
+            neighbour.append(((x    )%space_width, (y + 1)%space_lengtth))
+            neighbour.append(((x + 1)%space_width, (y - 1)%space_lengtth))
+            neighbour.append(((x + 1)%space_width, (y    )%space_lengtth))
+            neighbour.append(((x + 1)%space_width, (y + 1)%space_lengtth))
+        else:
+            neighbour.append((x - 1, y - 1))
+            neighbour.append((x - 1, y    ))
+            neighbour.append((x - 1, y + 1))
+            neighbour.append((x, y - 1    ))
+            neighbour.append((x, y + 1    ))
+            neighbour.append((x + 1, y - 1))
+            neighbour.append((x + 1, y    ))
+            neighbour.append((x + 1, y + 1))
     elif neighbour_rule == 'vonNeumann':
         neighbour    = []
-        neighbour.append(((x - 1)%space_width, (y    )%space_lengtth))
-        neighbour.append(((x + 1)%space_width, (y    )%space_lengtth))
-        neighbour.append(((x    )%space_width, (y + 1)%space_lengtth))
-        neighbour.append(((x    )%space_width, (y - 1)%space_lengtth))
+        if border_rule == 'snakelike':
+            neighbour.append(((x - 1)%space_width, (y    )%space_lengtth))
+            neighbour.append(((x + 1)%space_width, (y    )%space_lengtth))
+            neighbour.append(((x    )%space_width, (y + 1)%space_lengtth))
+            neighbour.append(((x    )%space_width, (y - 1)%space_lengtth))
+        else:
+            neighbour.append((x - 1, y))
+            neighbour.append((x + 1, y))
+            neighbour.append((x, y + 1))
+            neighbour.append((x, y - 1))
     return neighbour
 
 
@@ -84,18 +110,19 @@ def check_nieghbours(x, y):
 # while cell_empty in space:
 #     space = one_step(space)
 
-def f(x, y):
-    data = np.array([[[random.random(), random.random(), random.random()]
-                      for item in range(y)] for itemm in range(x)])
-    return data
-
-
-def updatefig(*args):
-    space = one_step(space)
-    return
-
-
-def run():
-    anim = animation.FuncAnimation(fig, one_step, interval=5, blit=True)
+# def f(x, y):
+#     data = np.array([[[random.random(), random.random(), random.random()]
+#                       for item in range(y)] for itemm in range(x)])
+#     return data
+#
+#
+# def updatefig(*args):
+#     global space
+#     im.set_array(one_step(space))
+#     return im,
+#
+#
+# def run():
+#     anim = animation.FuncAnimation(fig, one_step, interval=5, blit=True)
 
 
